0518617fba5f412ad1ffb5860a5e2539306d76db
Kenneth-Galvez-BSIT3N <57065417+PalindromeRotator@users.noreply.github.com>
2022-12-27 12:46:42 +0800

    UI Changes

--- job_portal/job_portal_app/urls.py
+++ job_portal/job_portal_app/urls.py
@@ -4,4 +4,14 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('user/main/', views.main, name='main'),
+    path('user/profile/', views.profile, name='profile'),
+    path('user/job_alert/', views.job_alert, name='job_alert'),
+    path('company/main/', views.main_company, name='main_company'),
+    path('company/add_job/', views.add_job, name='add_job_company'),
+    path('company/profile/', views.profile_company, name='profile_company'),
+    path('admin/main/', views.main_admin, name='main_admin'),
+    path('admin/job_seeker/', views.job_seeker, name='job_seeker'),
+    path('admin/company/', views.company, name='company'),
+    path('admin/job_application/', views.job_application, name='job_application'),
 ]
--- job_portal/job_portal_app/views.py
+++ job_portal/job_portal_app/views.py
@@ -5,3 +5,34 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def main(request):
+    return render(request, 'main/user/main_page.html')
+
+def profile(request):
+    return render(request, 'main/user/profile_page.html')
+
+
+def job_alert(request):
+    return render(request, 'main/user/job_alert_page.html')
+
+def main_company(request):
+    return render(request, 'main/company/main_page.html')
+
+def add_job(request):
+    return render(request, 'main/company/add_job_page.html')
+
+def profile_company(request):
+    return render(request, 'main/company/profile_page.html')
+
+def main_admin(request):
+    return render(request, 'main/admin/main_page.html')
+
+def job_seeker(request):
+    return render(request, 'main/admin/job_seeker_page.html')
+
+def company(request):
+    return render(request, 'main/admin/company_page.html')
+
+def job_application(request):
+    return render(request, 'main/admin/job_application_page.html')
